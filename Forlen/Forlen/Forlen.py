c612efb2798108eae190e329f81b05ff0650c619
mostovoygrafit <mneprostonuzhenlogin@gmail.com>
2023-05-02 21:06:16 +0300

    fixed buttons

--- Forlen/Forlen/Forlen.py
+++ Forlen/Forlen/Forlen.py
@@ -16,15 +16,15 @@ with open("Dictionary.txt", "r", encoding="utf-8") as file:
         esp_word, rus_word = line.strip().split('-')
         dictionary[esp_word.strip()] = rus_word.strip()
 
-def generate_markup():
+def generate_markup(mode):
     markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
-    markup.add("Начать игру")
-    markup.add("Выйти из игры")
-    markup.add("Я был(а) прав(а)")
-    markup.add("Добавить слова")
-    markup.add("Остановить добавление слов")
-    markup.add("Помощь")
-    markup.add("Об авторе")
+
+    if mode == "main":
+        markup.add("Начать игру", "Добавить слова", "Помощь", "Об авторе")
+    elif mode == "game":
+        markup.add("Выйти из игры", "Я был(а) прав(а)")
+    elif mode == "adding_words":
+        markup.add("Остановить добавление слов")
     return markup
 
 def start_game(chat_id):
@@ -33,23 +33,23 @@ def start_game(chat_id):
         return
     game_mode[chat_id] = True
     user_score[chat_id] = 0
-    bot.send_message(chat_id, "Игра началась!", reply_markup=generate_markup())
+    bot.send_message(chat_id, "Игра началась!", reply_markup=generate_markup("game"))
     send_word(chat_id)
     
 def send_help(chat_id):
-    bot.send_message(chat_id, "<i>Для старта игры нажмите кнопку 'Начать игру'. Бот начнет присылать вам слова на испанском, в ответе вам нужно написать перевод этого слова на русский. При правильном ответе вам начисляется 1 очко, при неправильном отнимается. В случае если бот несправедливо не зачел вам перевод, нажмите кнопку 'Я был(а) прав(а)'. Для выхода из игры нажмите кнопку 'Выйти из игры'. Для добавления слов в словарь нажмите кнопку 'Добавить слова' и введите слова с переводом через дефис. Чтобы остановить добавление слов, нажмите кнопку 'Остановить добавление слов'.</i>", parse_mode='HTML', reply_markup=generate_markup())
+    bot.send_message(chat_id, "<i>Чтобы начать игру, нажмите кнопку 'Начать игру'. Бот начнет отправлять вам слова на испанском языке, а ваша задача - написать перевод этого слова на русский. За каждый правильный ответ вы получаете 1 балл, а за каждый неправильный - теряете 1 балл. Если вы считаете, что бот несправедливо не засчитал ваш правильный ответ, нажмите кнопку 'Я был(а) прав(а)'. Чтобы закончить игру, нажмите кнопку 'Выйти из игры'. Если вы хотите добавить слова в словарь, нажмите кнопку 'Добавить слова' и введите слова с их переводом через дефис. Чтобы прекратить добавление слов, нажмите кнопку 'Остановить добавление слов'.</i>", parse_mode='HTML', reply_markup=generate_markup("main"))
     
 def send_author(chat_id):
-    bot.send_message(chat_id, "<i>Автор бота Елена Маркеева, создан с любовью к испанскому языку</i>", parse_mode='HTML', reply_markup=generate_markup())
+    bot.send_message(chat_id, "<i>Автор бота Елена Маркеева, создан с любовью к испанскому языку</i>", parse_mode='HTML', reply_markup=generate_markup("main"))
 
 def stop_game(chat_id):
     if chat_id in game_mode:
         del game_mode[chat_id]
         score = user_score[chat_id]
         del user_score[chat_id]
-        bot.send_message(chat_id, f"<b>Игра окончена! Ваш счет: {score}</b>", parse_mode='HTML', reply_markup=generate_markup())
+        bot.send_message(chat_id, f"<b>Игра окончена! Ваш счет: {score}</b>", parse_mode='HTML', reply_markup=generate_markup("main"))
     else:
-        bot.send_message(chat_id, "Игра не была начата. Используйте /start_game, чтобы начать.", reply_markup=generate_markup())
+        bot.send_message(chat_id, "Игра не была начата. Используйте /start_game, чтобы начать.", reply_markup=generate_markup("main"))
 
 @bot.message_handler(content_types=['text'], func=lambda message: True)
 def get_text_messages(message):
@@ -126,10 +126,10 @@ def button_handler(chat_id, user_input):
 
 def start_adding_words(chat_id):
     if chat_id in game_mode:
-        bot.send_message(chat_id, "Добавление слов невозможно в режиме игры.")
+        bot.send_message(chat_id, "Для добавления слов выйдите из режима игры.")
         return
     adding_words_mode[chat_id] = True
-    bot.send_message(chat_id, "Введите слово на испанском и его перевод на русском через дефис (например: casa-дом). Для выхода из режима добавления слов нажмите кнопку 'Остановить добавление слов'.", reply_markup=generate_markup())
+    bot.send_message(chat_id, "Введите слово на испанском и его перевод на русском через дефис (например: casa-дом). Для выхода из режима добавления слов нажмите кнопку 'Остановить добавление слов'.", reply_markup=generate_markup("adding_words"))
 
 def send_word(chat_id):
     word = random.choice(list(dictionary.keys()))
@@ -140,23 +140,23 @@ def send_word(chat_id):
 def stop_adding_words(chat_id):
     if chat_id in adding_words_mode:
         del adding_words_mode[chat_id]
-        bot.send_message(chat_id, "Вы вышли из режима добавления слов. Ваши слова сохранены для следующих сессий.", reply_markup=generate_markup())
+        bot.send_message(chat_id, "Вы вышли из режима добавления слов. Ваши слова сохранены для следующих сессий.", reply_markup=generate_markup("main"))
     else:
-        bot.send_message(chat_id, "Вы не находитесь в режиме добавления слов. Нажмите кнопку 'Добавить слова', чтобы начать добавление слов.", reply_markup=generate_markup())
+        bot.send_message(chat_id, "Вы не находитесь в режиме добавления слов. Нажмите кнопку 'Добавить слова', чтобы начать добавление слов.", reply_markup=generate_markup("main"))
 
 def handle_correct_answer(chat_id):
     if chat_id not in game_mode:
-        bot.send_message(chat_id, "Игра не была начата. Используйте /start_game, чтобы начать.", reply_markup=generate_markup())
+        bot.send_message(chat_id, "Игра не была начата. Используйте /start_game, чтобы начать.", reply_markup=generate_markup("main"))
         return
 
     if chat_id in user_was_wrong and user_was_wrong[chat_id] and chat_id in user_score:
         user_score[chat_id] += 2
-        bot.send_message(chat_id, f"Прошу прощения, вы были правы. Теперь ваш счет: {user_score[chat_id]}", reply_markup=generate_markup())
-        bot.send_message(chat_id, f"Переведите {last_word[chat_id]}", reply_markup=generate_markup())
+        bot.send_message(chat_id, f"Прошу прощения, вы были правы. Теперь ваш счет: {user_score[chat_id]}", reply_markup=generate_markup("game"))
+        bot.send_message(chat_id, f"Переведите {last_word[chat_id]}", reply_markup=generate_markup("game"))
         user_was_wrong[chat_id] = False
     elif chat_id in user_score:
         user_score[chat_id] -= 1
-        bot.send_message(chat_id, f"Не пытайтесь меня обмануть. Минус балл. Теперь ваш счет: {user_score[chat_id]}", reply_markup=generate_markup())
-        bot.send_message(chat_id, f"Переведите {last_word[chat_id]}", reply_markup=generate_markup())
+        bot.send_message(chat_id, f"Не пытайтесь меня обмануть, вы не совершили ошибку. Минус балл. Теперь ваш счет: {user_score[chat_id]}", reply_markup=generate_markup("game"))
+        bot.send_message(chat_id, f"Переведите {last_word[chat_id]}", reply_markup=generate_markup("game"))
 
 bot.polling(none_stop=True, interval=0)
